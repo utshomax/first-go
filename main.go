058120f7f347dd7244d5603f0713e5357de2b97e
utsab <utsab8008@gmail.com>
2023-03-12 06:19:31 +0530

    shuffle truely

--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 func main() {
 	cards := newDeck()
-	hand, remianingCards := deal(cards, 5)
-	hand.print()
-	remianingCards.print()
+	cards.print()
+	cards.shuffle()
+	cards.print()
+	//cards.saveToFile("My cards")
 }
